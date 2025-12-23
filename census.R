eef6c63c0fa834ebf21f4139ef8b09f4828640a8
“gabebrock” <gabebrock04@gmail.com>
2025-12-23 12:32:15 -0600

    lint census setup

--- census.R
+++ census.R
@@ -14,8 +14,8 @@ nyc_census2020_pct <- read_csv("r-data/nyc-census/nyc_precinct_2020pop.csv")
 nyc_census2020_pct$precinct <- sprintf("%03d", as.integer(nyc_census2020_pct$precinct))
 
 # nyc-census (only one race totals and total 2+ races)
-nyc_census2020_pct <- nyc_census2020_pct %>%
-  select(precinct, P1_001N, P1_002N, P1_003N, P1_004N, P1_005N, P1_006N, P1_007N, P1_008N, P1_009N, P2_002N, P2_003N) %>%
+nyc_census2020_pct <- nyc_census2020_pct |>
+  select(precinct, P1_001N, P1_002N, P1_003N, P1_004N, P1_005N, P1_006N, P1_007N, P1_008N, P1_009N, P2_002N, P2_003N) |>
   rename_with(~ case_match(
     .,
     "P1_001N" ~ "total_population",
@@ -33,16 +33,16 @@ nyc_census2020_pct <- nyc_census2020_pct %>%
   ))
 
 # precincts, proportion of black residents
-nyc_census2020_pct <- nyc_census2020_pct %>%
+nyc_census2020_pct <- nyc_census2020_pct |>
   mutate(black_prop = round(black_alone / total_population, 2),
          white_prop = round(white_alone / total_population, 2),
          hisp_prop = round(hispanic_or_latino / total_population, 2))
 
 # join nyc-census precinct-level data to spatial precinct data
-nyc_census2020_pct <- nyc_census2020_pct %>%
-  mutate(precinct = as.integer(precinct)) %>%
+nyc_census2020_pct <- nyc_census2020_pct |>
+  mutate(precinct = as.integer(precinct)) |>
   left_join(
-    nypd_sf %>%
+    nypd_sf |>
       mutate(Precinct = as.integer(Precinct)),
     by = c("precinct" = "Precinct")
   )
@@ -61,11 +61,11 @@ nyc_census2020_trc <- get_decennial(
     state = "NY",
     county = c("Bronx", "Kings", "New York", "Queens", "Richmond"),
     year = 2020,
-    geometry = TRUE) %>%
+    geometry = TRUE) |>
   pivot_wider(
     names_from = variable,
     values_from = value
-  ) %>%
+  ) |>
   rename_with(~ case_match(
     .,
     "P1_001N" ~ "total_population",
@@ -83,6 +83,4 @@ nyc_census2020_trc <- get_decennial(
   ))
 
 
-
-  
 .setup_complete_census <- TRUE
